082f2f485ef685cd81a3089b85b4138c825baabb
kcsongidev <kcsongidev@gmail.com>
2019-11-07 12:54:22 +0100

    config dev/prod with env and create server script

--- src/App.js
+++ src/App.js
@@ -8,10 +8,12 @@ import "./App.scss";
 const createdReactElement = React.createElement('p', null, 'React.createElement()');
 
 class App extends Component {
+
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <small>You are running this application in <b>{process.env.NODE_ENV}</b> mode.</small>
         {createdReactElement}
         <Comp />
         <PureComp />
