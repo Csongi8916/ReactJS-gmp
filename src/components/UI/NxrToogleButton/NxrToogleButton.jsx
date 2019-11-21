b56f376fc4ae0c3e3d4406df1b21955c52fcbaa0
Csongi8916 <38467520+Csongi8916@users.noreply.github.com>
2019-11-21 12:42:29 +0100

    Change button based solution to toogle button

--- src/components/UI/NxrToogleButton/NxrToogleButton.jsx
+++ src/components/UI/NxrToogleButton/NxrToogleButton.jsx
@@ -3,18 +3,24 @@ import React, { useState } from 'react';
 import classes from './NxrToogleButton.scss';
 
 export default function NxrToogleButton(props) {
-  const [isActiveLeft, setActive] = useState(true);
+  const [selectedOption, setSelectedOption] = useState(props.firstTitle);
+
+  const handleToogle = (data) => {
+    setSelectedOption(data);
+  }
 
   return (
     <div className="NxrToogle">
       <span className="NxrToogle_ModeTitle">{props.title}</span>
       <div className="NxrToogle_Container">
-        <div
-          onClick={() => setActive(true)}
-          className={`NxrToogle_Btn ${isActiveLeft ? 'NxrToogle_Btn-Active' : 'NxrToogle_Btn-Inactive'}`}>{props.firstTitle}</div>
-        <div
-          onClick={() => setActive(false)}
-          className={`NxrToogle_Btn ${isActiveLeft ? 'NxrToogle_Btn-Inactive' : 'NxrToogle_Btn-Active'}`}>{props.secondTitle}</div>
+        <button className={`NxrToogle_Btn ${selectedOption === props.firstTitle ? 'NxrToogle_Btn-Active' : 'NxrToogle_Btn-Inactive'}`}
+          onClick={() => handleToogle(props.firstTitle)}>
+          {props.firstTitle}
+        </button>
+        <button className={`NxrToogle_Btn ${selectedOption === props.secondTitle ? 'NxrToogle_Btn-Active' : 'NxrToogle_Btn-Inactive'}`}
+          onClick={() => handleToogle(props.secondTitle)}>
+          {props.secondTitle}
+        </button>
       </div>
     </div>
   )
